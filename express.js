45f20356ba6e9a27fb7e29287ec6466d28fdc0c4
Arifatul Ronansyah <v.fatul@gmail.com>
2018-12-05 21:26:03 +0700

    Add query string functionality

--- express.js
+++ express.js
@@ -12,16 +12,29 @@ var products = [
   "Product D",
   "Product E"
 ];
+var products = {
+  pages: {
+    1: ["Product A", "Product B", "Product C", "Product D", "Product E"],
+    2: ["Product F", "Product G", "Product H", "Product I", "Product J"]
+  }
+};
+
+app.use("/assets", express.static("assets"));
 
 app.get("/", function(req, res) {
+  console.log(req.query);
   res.status(200);
   res.render("index", { orang: req.params.nama, products });
 });
 app.get("/about", function(req, res) {
+  console.log(req.query);
+  var currentPage = req.query.page;
+  var createdAt = req.query.created_at;
   res.status(200);
-  res.render("about");
+  res.render("about", { page: currentPage, created: createdAt, products });
 });
 app.get("*", function(req, res) {
+  console.log(req.query);
   res.status(404);
   res.send("Page Not Found");
 });
